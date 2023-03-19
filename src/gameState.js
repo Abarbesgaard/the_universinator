f7e0210faa4a313266b31b7aa7ac8b338d61eaa3
Andrew Steele <andrew.steele@adhocteam.us>
2023-03-19 06:18:07 -0400

    Refactor to GameState

--- src/gameState.js
+++ src/gameState.js
@@ -7,8 +7,8 @@
 
 /**
  * @typedef {Object} GameState
- * @property {Map<number, LoggedText>} logs
- * @property {Map<number, LoggedText>} messages
+ * @property {Array<LoggedText>} logs
+ * @property {Array<LoggedText>} messages
  * @property {(logId: number) => LoggedText | undefined} getLogById
  * @property {(messageId: number) => LoggedText | undefined} getMessageById
  * @property {(logItem: LoggedText) => void} addLog
@@ -17,10 +17,10 @@
 
 /** @type {GameState} */
 export const GameState = {
-  logs: new Map(),
-  messages: new Map(),
-  getLogById: (logId) => GameState.logs.get(logId),
-  getMessageById: (messageId) => GameState.messages.get(messageId),
-  addLog: (logItem) => GameState.logs.set(logItem.id, logItem),
-  addMessage: (messageItem) => GameState.messages.set(messageItem.id, messageItem),
+  logs: [],
+  messages: [],
+  getLogById: (logId) => GameState.logs.find((log) => log.id === logId),
+  getMessageById: (messageId) => GameState.messages.find((message) => message.id === messageId),
+  addLog: (logItem) => GameState.logs.push(logItem),
+  addMessage: (messageItem) => GameState.messages.push(messageItem),
 };
